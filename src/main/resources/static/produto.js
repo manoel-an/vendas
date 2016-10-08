7c73b0bf83e1658909e715fcc2443e87337f5262
aluno <aluno@PT649456>
2016-10-08 14:20:40 -0300

    arquivos iniciais

---
+++ src/main/resources/static/produto.js
@@ -0,0 +1,22 @@
+angular.module("vendas").controller("novoProdutoCtrl", function($scope, $http) {
+
+	$scope.produtos = [];
+	
+	$scope.listarProduto = function() {
+		$http.post("produto/listar").success(function(data) {
+			$scope.produtos = data;
+			console.log(data);
+		});
+	};
+
+	$scope.novoProduto = function(produto) {
+		$http.post("produto/novo", produto).success(function(data) {
+			console.log(data);
+		this.listarProduto();
+		});
+	};
+	
+	$scope.editarProduto = function(produto) {
+		alert(produto.nome);
+	};
+});
